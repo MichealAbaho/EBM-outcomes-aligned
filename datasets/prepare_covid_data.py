725d567215b5e4ef41afae1af549fc60108746c4
michealabaho <michealabaho265@gmail.com>
2020-07-23 18:10:56 +0100

    prepare tsv classification input data

--- datasets/prepare_covid_data.py
+++ datasets/prepare_covid_data.py
@@ -26,9 +26,32 @@ def prepare_covid_data(args):
             count += 1
         cvd.close()
 
+def prepare_covid_quot_seperated_data(args):
+    covid_df = pd.read_excel(args.data)
+    count = 1
+    covid_df_processed = []
+    outcome = 'out_primary_measure' if args.pri_sec == 'primary_outcomes' else 'out_secondary_measure'
+    covid_df[outcome] = covid_df[outcome].astype(str)
+    study_id = covid_df.columns[0]
+    for sent in covid_df[[study_id,outcome]].values:
+        sent_split = sent[1].split(';')
+        sent_split = [i for i in sent_split if i]
+        for i,sub_sent in enumerate(sent_split):
+            if sub_sent != '\n' or sub_sent != '':
+                covid_df_processed.append((str(sent[0]).strip(), sub_sent.strip()))
+        count += 1
+    covid_df_processed_frame = pd.DataFrame(covid_df_processed, columns=['study_id', 'outcome'])
+    covid_df_processed_frame.to_csv('{}_quot.csv'.format(args.pri_sec))
+
+
+
 if __name__=='__main__':
     par = argparse.ArgumentParser()
     par.add_argument('--data', default='covid.xlsx', type=str, help='source of dataset')
+    par.add_argument('--quot_separated_outcomes', action='store_true', help='dataset cotains outcomes however they are separated by quotation marks')
     par.add_argument('--pri_sec', default='primary_outcomes', help='primary outcomes or secondary outcomes')
     args = par.parse_args()
-    prepare_covid_data(args)
+    if not args.quot_separated_outcomes:
+        prepare_covid_data2(args)
+    else:
+        prepare_covid_quot_seperated_data(args)
