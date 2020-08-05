baea793883ff10351013e874548c7729d18016bc
Micheal Abaho <michealabaho265@gmail.com>
2020-08-05 20:05:57 +0100

    Fixed the bug unequal token and tag lists

--- multilabe_data_preprocess.py
+++ multilabe_data_preprocess.py
@@ -109,10 +109,7 @@ class data_load:
 
     def read_ebm_nlp(self, ebm_nlp):
         sentence = []
-        r = open('{}/wrong.txt'.format(dest_folder), 'w')
-        # with open(self.ebm_nlp, 'r') as open_f, \
-        #         open('{}/ebm_nlp_multilabels.txt'.format(self.dest_folder), 'w') as file, \
-        #         open('{}/ebm_nlp_multilabels.json'.format(self.dest_folder), 'w') as js:
+
         data, ground_truth = [], {}
         for i in ebm_nlp:
             if i.__contains__('.txt'):
@@ -120,134 +117,163 @@ class data_load:
                 data += open_f.readlines()
             elif i.__contains__('.xlsx'):
                 ground_truth = i
-        #         ground_truth_csv = pd.read_csv(i)
-        #         labels = set(ground_truth_csv['Label'])
-        #         for label in labels:
-        #             label_ = ground_truth_csv.loc[ground_truth_csv['Label'] == label.strip()]
-        #             ground_truth[label.lower()] = list(label_['Outcome'])
-        #
-        # print(ground_truth.keys())
-        # outcomes_list = [i for j in list(ground_truth.values()) for i in j]
-        # for x,y in ground_truth.items():
-        #     r.write('+++'+x+'+++\n')
-        #     for i in y:
-        #         r.write('\n{}'.format(i))
-        #     r.write('\n')
+
         sheets = pd.ExcelFile(ground_truth)
         f = 0
-        for i in data:
-            if i != '\n':
-                i = i.split()
-                sentence.append((i[0], i[1]))
-            elif i == '\n':
-                sent_unpacked = [i[0] for i in sentence]
-                tag_unpacked = [i[1] for i in sentence]
-                d = k = 0
-                #print(' '.join(sent_unpacked))
-                sent_tokens, tag_tokens = [], []
-                # process each word in a sentence, looking for those that form outcome phrases and obtain a vector representation for entire outcome phrase,
-                for i in range(len(sent_unpacked)):
-                    if i == d:
-                        if tag_unpacked[i].startswith('B-'):
-                            #print(tag_unpacked[i])
-                            out_domain = tag_unpacked[i][2:].lower().strip()
-                            z, z_indexes = sent_unpacked[i], [i]
-                            sent_tokens.append(sent_unpacked[i])
-                            #tag_tokens.append(tag_unpacked[i][0]+'-'+tag_unpacked[i][2:].lower())
-                            for j in range(i + 1, len(sent_unpacked)):
-                                if tag_unpacked[j].startswith('I-'):
-                                    z += ' {}'.format(sent_unpacked[j])
-                                    z_indexes.append(j)
-                                    sent_tokens.append((sent_unpacked[j]))
-                                    #tag_tokens.append(tag_unpacked[j][0]+'-'+tag_unpacked[j][2:].lower())
-                                    d = j
-                                else:
-                                    break
+        with open('{}/ebm_nlp_multilabels.txt'.format(self.dest_folder), 'w') as file, \
+                open('{}/ebm_nlp_multilabels.json'.format(self.dest_folder), 'w') as js:
+            json_output = []
+            for i in data:
+                if i != '\n':
+                    i = i.split()
+                    sentence.append((i[0], i[1]))
+                elif i == '\n':
+                    sent_unpacked = [i[0] for i in sentence]
+                    tag_unpacked = [i[1] for i in sentence]
+                    outcome_domain, outcomes, json_sent = [], [], {}
+                    d = k = 0
+                    #print(' '.join(sent_unpacked))
+                    sent_tokens, tag_tokens = [], []
+                    # process each word in a sentence, looking for those that form outcome phrases and obtain a vector representation for entire outcome phrase,
+                    for i in range(len(sent_unpacked)):
+                        if i == d:
+                            if tag_unpacked[i].startswith('B-'):
+                                #print(tag_unpacked[i])
+                                out_domain = tag_unpacked[i][2:].lower().strip()
+                                z, z_indexes = sent_unpacked[i], [i]
+                                sent_tokens.append(sent_unpacked[i])
+                                tag_tokens.append(tag_unpacked[i])
+                                for j in range(i + 1, len(sent_unpacked)):
+                                    if tag_unpacked[j].startswith('I-'):
+                                        z += ' {}'.format(sent_unpacked[j])
+                                        z_indexes.append(j)
+                                        sent_tokens.append((sent_unpacked[j]))
+                                        tag_tokens.append(tag_unpacked[j])
+                                        d = j
+                                    else:
+                                        break
 
-                            primary_domain_ebm_nlp = []
-                            sht_names = sheets.sheet_names
+                                primary_domain_ebm_nlp = []
+                                sht_names = sheets.sheet_names
 
-                            b = False
-                            #print(z, out_domain)
-                            for sht in sht_names:
-                                df_multiple_domains = sheets.parse(sht)
-                                df_multiple_domains = df_multiple_domains.loc[df_multiple_domains['Label'].str.lower() == sht.strip().lower()]
-                                if sht.strip().lower() == out_domain:
-                                    for y in df_multiple_domains.iloc[:,2:].values.tolist():
-                                        y = [str(i) for i in y]
-                                        label = re_label_nlp(out_domain)
-                                        if y[0].strip() == z.strip():
-                                            #extracted outcome is not an outcome
-                                            if all(i=='nan' for i in y[1:]):
-                                                print(1, y, out_domain)
-                                                for m, n in enumerate(z_indexes):
-                                                    tag_tokens.append('O')
-                                            #the outcome is fine
-                                            elif y[1] == z.strip():
-                                                print(2, z, y[1], out_domain)
-                                                m = 0
-                                                for n in z_indexes:
-                                                    if m == 0:
-                                                        tag_tokens.append('B-{}'.format(label))
-                                                        m += 1
-                                                    else:
-                                                        tag_tokens.append('I-{}'.format(label))
-                                            #corrected outcome is different from original outcome
-                                            elif y[1] != 'nan' and y[1] != z.strip():
-                                                print(3, z, y[1], out_domain)
-                                                z_ = y[1].strip().split()
-                                                m = 0
-                                                for n in z_indexes:
-                                                    if tag_unpacked[n] == z_[m]:
-                                                        if tag_unpacked[n] == z_[0]:
-                                                            tag_tokens.append('B-{}'.format(label))
-                                                        else:
-                                                            tag_tokens.append('I-{}'.format(label))
-                                                        m += 1
-                                                    else:
-                                                        tag_tokens.append('O')
-                                            elif y[1] == 'nan':
-                                                print(4, z, y[2:], out_domain)
-                                                y_ = [i for i in y[2:] if i != 'nan']
-                                                for u in y_:
-                                                    z_ = u.strip().split()
-                                                    label = core_areas[y_.index(u)]
+                                b = False
+                                for sht in sht_names:
+                                    df_multiple_domains = sheets.parse(sht)
+                                    df_multiple_domains = df_multiple_domains.loc[df_multiple_domains['Label'].str.lower() == sht.strip().lower()]
+                                    # print('\n',sht.strip().lower(), out_domain, sht.strip().lower()==out_domain.lower())
+
+                                    if sht.strip().lower() == out_domain:
+                                        for y in df_multiple_domains.iloc[:,2:].values.tolist():
+                                            y = [str(i) for i in y]
+
+                                            label = re_label_nlp(out_domain)
+                                            if y[0].strip() == z.strip():
+                                                z_split = z.split()
+                                                #extracted outcome is not an outcome
+                                                if all(i=='nan' for i in y[1:]):
+                                                    for m, n in enumerate(z_indexes):
+                                                        tag_tokens[n] = 'O'
+                                                #the outcome is fine
+                                                elif y[1] == z.strip():
                                                     m = 0
                                                     for n in z_indexes:
-                                                        if tag_unpacked[n] == z_[m]:
-                                                            if tag_unpacked[n] == z_[0]:
-                                                                tag_tokens.append('B-{}'.format(label))
-                                                            else:
-                                                                tag_tokens.append('I-{}'.format(label))
+                                                        if m == 0:
+                                                            tag_tokens[n] = 'B-{}'.format(label)
                                                             m += 1
                                                         else:
-                                                            tag_tokens.append('O')
-                                            b = True
-                                            break
+                                                            tag_tokens[n] = 'I-{}'.format(label)
+                                                    outcomes.append(z.strip())
+                                                    outcome_domain.append(label)
+                                                #corrected outcome is different from original outcome
+                                                elif y[1] != 'nan' and y[1] != z.strip():
+                                                    y_ = y[1].split(';')
+                                                    if len(y_) > 1:
+                                                        pass
+                                                    for u in y_:
+                                                        z_ = u.strip().split()
+                                                        m = 0
+                                                        nn = []
+                                                        for n in z_indexes:
+                                                            if sent_unpacked[n] in z_:
+                                                                if sent_unpacked[n] == z_[0]:
+                                                                    tag_tokens[n] = 'B-{}'.format(label)
+                                                                else:
+                                                                    tag_tokens[n] = 'I-{}'.format(label)
+                                                                m += 1
+                                                            elif sent_unpacked[n] in [i for j in [i.split() for i in y_] for i in j]:
+                                                                pass
+                                                            else:
+                                                                nn.append(n)
+                                                                tag_tokens[n] = 'O'
+                                                        outcomes.append(u.strip())
+                                                        outcome_domain.append(label)
+                                                elif y[1] == 'nan':
+                                                    y_ = [i for i in y[2:] if i != 'nan']
+                                                    for u in y_:
+                                                        z_ = u.split(';')
+                                                        label_ = core_areas[y[2:].index(u)]
+                                                        if len(z_) > 1:
+                                                            pass #print('\t\t\t\t\t\t:-two instead of 1 outcome', z_)
+                                                        for u_ in z_:
+                                                            z__ = u_.strip().split()
+                                                            m = 0
+                                                            nn = []
+                                                            print(4.3, z__)
+                                                            for n in z_indexes:
+                                                                if sent_unpacked[n] in z__:
+                                                                    if sent_unpacked[n] == z__[0]:
+                                                                        tag_tokens[n] = 'B-{}'.format(label_)
+                                                                    else:
+                                                                        tag_tokens[n] = 'I-{}'.format(label_)
+                                                                    m += 1
+                                                                elif sent_unpacked[n] in [i for j in [i.split() for i in z_] for i in j]:
+                                                                    pass
+                                                                else:
+                                                                    nn.append(n)
+                                                                    tag_tokens[n] = 'O'
+                                                            outcomes.append(u_.strip())
+                                                        outcome_domain.append(label_)
+                                                b = True
+                                                break
 
-                                    if b:
-                                        break
-                            print(sent_tokens)
-                            print(tag_tokens)
-                        else:
-                            sent_tokens.append(sent_unpacked[i])
-                            tag_tokens.append(tag_unpacked[i])
-                            if tag_unpacked[i] != '0':
-                                print('----------------{}----------------')
-                                break
-                        d += 1
-                if len(sent_tokens) != len(tag_tokens):
-                    print('\n\n\n\n\nSomething is wrong\n\n\n\n\n')
-                    print(sent_tokens)
-                    print(tag_tokens)
-                    break
-                # else:
-                #     print(sent_tokens)
-                #     print(tag_tokens)
-                sentence.clear()
-                f += 1
-                if f == 30:
-                    pass
+                                        if b:
+                                            break
+                                print(sent_tokens)
+                                print(tag_tokens)
+                            else:
+                                sent_tokens.append(sent_unpacked[i])
+                                tag_tokens.append(tag_unpacked[i])
+                                if tag_unpacked[i] != '0':
+                                    print('----------------{}----------------')
+                                    break
+                            d += 1
+                    if len(sent_tokens) != len(tag_tokens):
+                        print('\n\n\n\n\nSomething is wrong\n\n\n\n\n')
+                        print(sent_tokens)
+                        print(tag_tokens)
+                        break
+                    else:
+                        for x,y in zip(sent_tokens, tag_tokens):
+                            if y.startswith('B') or y.startswith('I'):
+                                y = y.split('-', 1)
+                                file.write('{} {}-outcome {}\n'.format(x, y[0], y[1:]))
+                            else:
+                                file.write('{} {}\n'.format(x, y))
+                        file.write('{}\n\n'.format(outcome_domain))
+
+                        json_sent['sentence'] = ' '.join(sent_tokens)
+                        json_sent['outcomes'] = outcomes
+                        json_sent['multi-labels'] = outcome_domain
+                        json_output.append(json_sent)
+                        print(sent_tokens)
+                        print(tag_tokens)
+                    sentence.clear()
+                    # f += 1
+                    # if f == 30:
+                    #     pass
+            json.dump(json_output, js, indent=1)
+            file.close()
+            js.close()
 
 def fetch_taxonomy_label(annotation_label):
     core_areas, COMET_LABELS = taxonomy.comet_taxonomy()  # taxonomy of outcomes
